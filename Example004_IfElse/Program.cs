6a19251b96c5c44f73bf51c26d66903faa85a327
fidel.alex <alekseifidel@gmail.com>
2023-07-30 19:33:44 +0500

    Exaple004_If_else

---
+++ Example004_IfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите имя пользователья");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "маша")
+    System.Console.WriteLine("Привет, это же Маша))");
+
+else
+    System.Console.WriteLine($"Привет, {userName}!");

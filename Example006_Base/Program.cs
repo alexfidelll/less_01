92c722f80e23090e8b6469b4dae73c605e8684c9
fidel.alex <alekseifidel@gmail.com>
2023-07-30 19:55:51 +0500

    Example006_base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+System.Console.WriteLine($"Максимально значение = {max}");
